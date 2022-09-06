60ee2022f93ebfc20f1affe550f051b0d3b7c9ad
Subhajit <shubhajit.pramanik@edudigm.in>
2022-09-06 16:47:07 -0700

    Code commited for first push

---
+++ resources/js/components/auth/LoginComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><h1>Login Component</h1></div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
